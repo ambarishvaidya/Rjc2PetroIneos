2d0f743b1dbc99f610133300f31815ec6587212a
ambarishvaidya <132093368+ambarishvaidya@users.noreply.github.com>
2026-05-24 17:46:34 +0100

    Adding datetime input tests 6. This extract does not have to run exactly on the minute and can be within +/- 1 minute of the configured interval.

--- PetroIneosServices/TradePositionAggregator/TradePositionAggregator.cs
+++ PetroIneosServices/TradePositionAggregator/TradePositionAggregator.cs
@@ -5,13 +5,31 @@ namespace TradePositionData;
 
 public class TradePositionAggregator : ITradePositionDataProvider<PowerPeriod>
 {
+    private int tolerance = 1; 
+
     public IEnumerable<PowerPeriod> GetTradePositions(DateTime localDateTime)
     {
-        throw new NotImplementedException();
+        if(!IsPassedLocalDateTimeValid(localDateTime))         
+            throw new ArgumentException("The date cannot be in the past.");
+
+        return null;
     }
 
     public Task<IEnumerable<PowerPeriod>> GetTradePositionsAsync(DateTime localDateTime)
     {
-        throw new NotImplementedException();
+        if (!IsPassedLocalDateTimeValid(localDateTime))        
+            throw new ArgumentException("The date cannot be in the past.");
+
+        return Task.FromResult<IEnumerable<PowerPeriod>>(null);
+    }
+
+    private bool IsPassedLocalDateTimeValid(DateTime localDateTime)
+    {
+        var now = DateTime.Now;
+
+        var lt = new DateTime(localDateTime.Year, localDateTime.Month, localDateTime.Day, localDateTime.Hour, localDateTime.Minute, localDateTime.Second);
+        var nt = new DateTime(now.Year, now.Month, now.Day, now.Hour, now.Minute, now.Second);
+
+        return (nt - lt).Duration() <= TimeSpan.FromMinutes(tolerance);
     }
 }
--- PetroIneosServices/TradePositionData.Tests/TradePositionAggregatorTests.cs
+++ PetroIneosServices/TradePositionData.Tests/TradePositionAggregatorTests.cs
@@ -1,9 +1,45 @@
 ﻿namespace TradePositionData.Tests;
 
-public class Tests
-{
+public class DateTimeInputTests
+{    
     [SetUp]
     public void Setup()
     {
+    }
+
+    [Test]
+    public void GetTradePositions_WhenDateIsInPast_ThrowArgumentException()
+    {
+        var agregator = new TradePositionAggregator();
+        var pastDate = DateTime.Now.AddDays(-1);
+        Assert.Throws<ArgumentException>(() => agregator.GetTradePositions(pastDate));
+    }
+
+    [Test]
+    public async Task GetTradePositionsAsync_WhenDateIsInPast_ThrowArgumentException()
+    {
+        var agregator = new TradePositionAggregator();
+        var pastDate = DateTime.Now.AddDays(-1);
+        Assert.ThrowsAsync<ArgumentException>(async () => await agregator.GetTradePositionsAsync(pastDate));
+    }
+
+    [TestCase(-1)]
+    [TestCase(0)]
+    [TestCase(1)]
+    public void GetTradePositions_WhenDateIsWithinTolerance_DoesNotThrowArgumentException(int min)
+    {
+        var agregator = new TradePositionAggregator();
+        var pastDate = DateTime.Now.AddMinutes(min);
+        Assert.DoesNotThrow(() => agregator.GetTradePositions(pastDate));
+    }
+
+    [TestCase(-1)]
+    [TestCase(0)]
+    [TestCase(1)]
+    public async Task GetTradePositionsAsync_WhenDateIsWithinTolerance_DoesNotThrowArgumentException(int min)
+    {
+        var agregator = new TradePositionAggregator();
+        var pastDate = DateTime.Now.AddMinutes(min);
+        Assert.DoesNotThrowAsync(async () => await agregator.GetTradePositionsAsync(pastDate));
     }
 }
