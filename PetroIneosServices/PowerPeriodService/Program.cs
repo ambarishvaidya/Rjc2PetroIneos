dc789177491956dff19724b77a2c6dca62b46386
ambarishvaidya <132093368+ambarishvaidya@users.noreply.github.com>
2026-05-25 15:09:08 +0100

    Configuring infrastructure.

--- PetroIneosServices/PowerPeriodService/Program.cs
+++ PetroIneosServices/PowerPeriodService/Program.cs
@@ -1,7 +1,12 @@
 using PowerPeriodService;
+using PowerPeriodInterface;
+using TradePositionData;
+using Services;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<Worker>();
+builder.Services.AddSingleton<IPowerService, PowerService>();
+builder.Services.AddTransient<ITradePositionDataProvider<IAggregatedTradePosition>, TradePositionAggregator>();
 
 var host = builder.Build();
 host.Run();
--- PetroIneosServices/PowerPeriodService/Worker.cs
+++ PetroIneosServices/PowerPeriodService/Worker.cs
@@ -1,14 +1,22 @@
+using PowerPeriodInterface;
+
 namespace PowerPeriodService;
 
-public class Worker(ILogger<Worker> logger) : BackgroundService
+public class Worker(ILogger<Worker> logger, ITradePositionDataProvider<IAggregatedTradePosition> tradePositionDataProvider) : BackgroundService
 {
+    
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         while (!stoppingToken.IsCancellationRequested)
         {
-            if (logger.IsEnabled(LogLevel.Information))
+            var resp = tradePositionDataProvider.GetTradePositions(DateTime.Now);
+            logger.LogInformation("Worker running at: {time} with response: {response}", DateTimeOffset.Now, resp);
+            if(resp.Status == AggregatedTradePositionStatus.Success)
             {
-                logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                foreach (var kvp in resp.TradePositions)
+                {
+                    logger.LogInformation("Trade Position: {key} with value: {value}", kvp.Key, kvp.Value);
+                }
             }
             await Task.Delay(1000, stoppingToken);
         }
