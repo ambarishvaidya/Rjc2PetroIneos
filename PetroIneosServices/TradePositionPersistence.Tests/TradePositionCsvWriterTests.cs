448fc1c0f5fbfc0f56c5f80fac5d68588ff88160
ambarishvaidya <132093368+ambarishvaidya@users.noreply.github.com>
2026-05-26 08:49:01 +0100

    Updating Persistence tests

--- PetroIneosServices/TradePositionPersistence.Tests/TradePositionCsvWriterTests.cs
+++ PetroIneosServices/TradePositionPersistence.Tests/TradePositionCsvWriterTests.cs
@@ -2,24 +2,52 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Moq;
-using PowerPeriodInterface;
 
 namespace TradePositionPersistence.Tests;
 
+public class TestCsvFolderPath
+{
+    Mock<ILogger<TradePositionCsvWriter>> _loggerMock;    
+
+    [SetUp]
+    public void Setup()
+    {
+        _loggerMock = new Mock<ILogger<TradePositionCsvWriter>>();  
+    }
+
+    [TestCase(null)]
+    [TestCase("Abc")]
+    [TestCase("")]
+    [TestCase(@"G:\Dopamine")]
+    public void Constructor_WhenCalledWithInvalidInputs_ThrowsArgumentException(string? invalidPath)
+    {
+        var configuration = new ConfigurationBuilder()
+            .AddInMemoryCollection(new Dictionary<string, string?>
+            {
+                { "CsvPowerPositionPath", invalidPath }
+            })
+            .Build();
+        
+        Assert.Throws<ArgumentException>(() => new TradePositionCsvWriter(_loggerMock.Object, configuration));
+    }
+}
+
 public class TestFileNameCreation
 {
-    Mock<ILogger<TradePositionCsvWriter>> _loggerMock;
-    Mock<IAggregatedTradePosition> _positionMock;
-    Mock<IConfiguration> _configurationMock;
+    Mock<ILogger<TradePositionCsvWriter>> _loggerMock;    
     TradePositionCsvWriter _writer;
 
     [SetUp]
     public void Setup()
     {
-        _loggerMock = new Mock<ILogger<TradePositionCsvWriter>>();
-        _positionMock = new Mock<IAggregatedTradePosition>();
-        _configurationMock = new Mock<IConfiguration>();
-        _writer = new TradePositionCsvWriter(_loggerMock.Object, _configurationMock.Object);
+        _loggerMock = new Mock<ILogger<TradePositionCsvWriter>>();        
+        var configuration = new ConfigurationBuilder()
+            .AddInMemoryCollection(new Dictionary<string, string>
+            {
+                { "CsvPowerPositionPath", "C:\\Temp" }
+            })
+            .Build();
+        _writer = new TradePositionCsvWriter(_loggerMock.Object, configuration);
     }
 
     [TestCase(2024, 12, 1, 12, 30, 59, "PowerPosition_20241201_1230.csv")]
